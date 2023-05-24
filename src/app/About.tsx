d0097a837e30d68d1f6747281e61dbf25129eb9b
JuanPrato <pratojuanmanuel2@gmail.com>
2023-05-24 09:53:41 -0300

    About page

---
+++ src/app/About.tsx
@@ -0,0 +1,27 @@
+import { Separator } from "@/components/Utils";
+import Button from "@/components/button";
+import Image from "next/image";
+
+export default function About() {
+
+  return (
+    <section className="grid grid-cols-2 px-36 py-10">
+      <h2 className="text-3xl font-semibold">NUESTRO SALON</h2>
+      <div>
+        <p className="mb-4 text-sm">Bienvenidos a Belleza Express, tu destino de belleza y bienestar en el corazón de la ciudad. En nuestra peluquería y centro de estética, nos apasiona realzar la belleza natural de nuestros clientes y brindarles una experiencia única y satisfactoria.</p>
+        <Button>LEER MÁS</Button>
+      </div>
+      <div className="text-center col-span-2 py-10">
+        <Image
+          className="shadow-lg rounded transition-transform hover:scale-105"
+          src="/salon.png"
+          alt="salon image"
+          width={1500}
+          height={1500}
+        />
+      </div>
+      <Separator />
+    </section>
+  );
+
+}
---
+++ src/components/Utils.tsx
@@ -0,0 +1,6 @@
+export function Separator() {
+  return (
+    <div className="col-span-2 w-[500px] m-auto border border-gray-300">
+    </div>
+  );
+}
