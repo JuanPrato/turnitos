72e8b88a5e6bd31831dd388cf8b4b1a827dd0894
JuanPrato <pratojuanmanuel2@gmail.com>
2023-05-30 18:09:36 -0300

    extract nav

---
+++ src/components/header/Nav.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="px-3 bg-black text-white">
+      <div className='flex justify-between items-center max-w-7xl m-auto'>
+        <Link href="/" className="p-3 text-xl font-semibold w-1/4 cursor-pointer"><h1>Belleza Express</h1></Link>
+        <section className="flex justify-evenly grow text-sm">
+          <Link href="" className='p-2'>HOME</Link>
+          <Link href="" className='p-2'>ABOUT</Link>
+          <Link href="" className='p-2'>SERVICES</Link>
+          <Link href="" className='p-2'>OURSTAFF</Link>
+        </section>
+        <Link href="/turn" className='w-1/4 px-5 text-center font-semibold'>AGENDA TU TURNO</Link>
+      </div>
+    </nav>
+  );
+}
