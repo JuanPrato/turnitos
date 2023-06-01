cbc47074b9d8a144a20290f4efd47163e58e6698
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-01 12:21:00 -0300

    pass className

--- src/components/Font.tsx
+++ src/components/Font.tsx
@@ -1,5 +1,7 @@
-export function SectionTitle({ text }: { text: string }) {
+import { twMerge } from "tailwind-merge"
+
+export function SectionTitle({ text, className }: { text: string, className?: string }) {
   return (
-    <h2 className="text-center text-3xl font-semibold">{text}</h2>
+    <h2 className={twMerge("text-center text-3xl font-semibold", className)}>{text}</h2>
   )
 }
