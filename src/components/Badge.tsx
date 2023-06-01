dec4a15e00b561ddb4115593ce34e94ffaa9893a
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-01 12:20:36 -0300

    add onClick to badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function Badge({ children }: { children: React.ReactNode }) {
+export function Badge({ children, onClick }: { children: React.ReactNode, onClick?: () => void }) {
   return (
-    <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-sm font-medium text-green-800 ring-1 ring-inset ring-green-600/20">
+    <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-sm font-medium text-green-800 ring-1 ring-inset ring-green-600/20" onClick={onClick}>
       {children}
     </span>
   )
