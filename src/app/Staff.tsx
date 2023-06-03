f3ff7e14358c765ed5153b581608cef2f6d40d52
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-03 14:41:06 -0300

    add staff carousel

--- src/app/Staff.tsx
+++ src/app/Staff.tsx
@@ -1,6 +1,11 @@
+"use client"
+
 import { SectionTitle } from "@/components/Font";
 import StaffCard from "@/components/StaffCard";
 import { Separator } from "@/components/Utils";
+import { useCarousel } from "@/hooks/useCarousel";
+import React, { Ref, RefObject, useEffect, useRef, useState } from "react";
+import { findDOMNode } from "react-dom";
 
 export type Employee = {
   name: string;
@@ -20,17 +25,49 @@ const STAFF: Employee[] = [
     name: "Ana Torres",
     specialty: "Coloring and highlights",
   },
+  {
+    name: "Ana Torres",
+    specialty: "Coloring and highlights",
+  },
+  {
+    name: "María López",
+    specialty: "Cuts and coloring",
+  },
+  {
+    name: "Carlos Rodríguez",
+    specialty: "Versatile stylist",
+  },
+  {
+    name: "Ana Torres",
+    specialty: "Coloring and highlights",
+  },
+  {
+    name: "Ana Torres",
+    specialty: "Coloring and highlights",
+  },
 ];
 
 export default function Staff() {
+
+  const { setElements, onScroll } = useCarousel<HTMLDivElement>({ offsetLeft: 1, offsetRigth: 2 });
+
   return (
     <>
-      <section className="py-10" id="staff">
+      <section className="py-10 px-20 relative" id="staff">
         <SectionTitle text="NUESTRO EQUIPO" />
-        <div className="flex justify-center gap-5 pt-5">
+        <div className="carousel carousel-center flex gap-5 pt-5 px-3">
           {
-            STAFF.map((e, i) => (<StaffCard key={i} employee={e} />))
+            STAFF.map((e, i) => (
+              <StaffCard
+                key={i}
+                employee={e}
+                executeWithRef={(r) => setElements(rs => [...rs, r])} />
+            ))
           }
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <button className="btn btn-circle" onClick={() => onScroll(-1)}>❮</button>
+          <button className="btn btn-circle" onClick={() => onScroll(+1)}>❯</button>
         </div>
       </section>
       <Separator />
--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -1,14 +1,26 @@
+"use client"
+
 import { Employee } from "@/app/Staff"
+import { RefObject, useEffect, useRef } from "react";
 
 interface Props {
   employee: Employee;
+  executeWithRef: (ref: RefObject<HTMLDivElement>) => void
 }
 
-export default function StaffCard({ employee }: Props) {
+export default function StaffCard({ employee, executeWithRef }: Props) {
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    executeWithRef(ref);
+  }, []);
+
   return (
     <div
-      className="aspect-[9/12] p-2 w-1/4 relative shadow-lg bg-cover flex flex-col justify-between items-start transition-transform hover:scale-105"
+      className="carousel-item aspect-[9/12] p-2 h-[400px] relative shadow-lg bg-cover flex flex-col justify-between items-start transition-transform"
       style={{ backgroundImage: "url(/staff.png)" }}
+      ref={ref}
     >
       <div className="bg-black text-white text-center p-2 absolute -top-2 -left-2">
         <h4 className="font-semibold text-lg">{employee.name}</h4>
