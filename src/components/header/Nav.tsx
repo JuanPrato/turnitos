2a133831f3a5204b62d3874a3efb9207ad7d8f91
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-03 14:39:10 -0300

    next link on header

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -7,9 +7,9 @@ export default function Nav() {
         <Link href="/" className="p-3 text-xl font-semibold w-1/4 cursor-pointer"><h1>Belleza Express</h1></Link>
         <section className="flex justify-evenly grow text-sm">
           <Link href="/" className='p-2'>HOME</Link>
-          <a href="#about" className='p-2'>ABOUT</a>
-          <a href="#services" className='p-2'>SERVICES</a>
-          <a href="#staff" className='p-2'>OURSTAFF</a>
+          <Link href="/#about" className='p-2'>ABOUT</Link>
+          <Link href="/#services" className='p-2'>SERVICES</Link>
+          <Link href="/#staff" className='p-2'>OURSTAFF</Link>
         </section>
         <Link href="/turn" className='w-1/4 px-5 text-center font-semibold'>AGENDA TU TURNO</Link>
       </div>
