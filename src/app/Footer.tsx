a550f14e8b016ac6d477c010c97e96ea41c589e6
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-03 14:39:43 -0300

    add link to admin panel

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,18 +5,20 @@ import Link from "next/link";
 export default function Footer() {
 
   return (
-    <footer className="bg-black text-white p-5 box-content grid grid-cols-2">
-      <section>
-        <h2 className="text-xl my-2">CONTACT INFO</h2>
-        <h3>🕛 Lun - Dom 9:00 AM - 19:00 PM</h3>
-        <h3>🗺️ Algun lugar - CABA</h3>
-        <h3>📧 algun@correo.com.ar</h3>
-        <h3>📱 11 6678 - 2928</h3>
-      </section>
-      <div className="grid items-center">
-        <BackToTopButton />
+    <footer className="bg-black text-white p-5 mt-10 box-content">
+      <div className="grid grid-cols-2 max-w-7xl mx-auto">
+        <section>
+          <h2 className="text-xl my-2">CONTACT INFO</h2>
+          <h3>🕛 Lun - Dom 9:00 AM - 19:00 PM</h3>
+          <h3>🗺️ Algun lugar - CABA</h3>
+          <h3>📧 algun@correo.com.ar</h3>
+          <h3>📱 11 6678 - 2928</h3>
+        </section>
+        <div className="grid items-center">
+          <BackToTopButton />
+        </div>
+        <Link className="text-center col-span-2" href="/admin">ENTRAR</Link>
       </div>
-      <Link className="text-center col-span-2" href="">ENTRAR</Link>
     </footer>
   );
 
