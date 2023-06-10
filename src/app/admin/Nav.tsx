fb0c6bcdb1602fae9b474152f02395f9a39d1b24
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-10 00:05:25 -0300

    put nav logic on component

--- src/app/admin/Nav.tsx
+++ src/app/admin/Nav.tsx
@@ -1,10 +1,18 @@
+import { PATHS } from "@/utils/constants";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Nav() {
+export default async function Nav() {
+
+  const supabase = createServerComponentClient({ cookies });
+  const { data: { session } } = await supabase.auth.getSession();
+  if (!session) return;
 
   return (
     <nav className="p-2 bg-black grid grid-cols-3 items-center place-content-center text-white">
-      <Link href="/admin/dashboard/">PETICIONES</Link>
+      <Link href={PATHS.ADMIN_DASHBOARD}>PETICIONES</Link>
+      <Link href={PATHS.ADMIN_CALENDAR}>CALENDARIO DE TURNOS</Link>
     </nav>
   )
 }
